3e87d89d8cae6294fbb41fde72049410d449e787
Ruslan Kavetsky <rusya.182@gmail.com>
2013-02-14 15:05:29 +0400

    Change copyright

--- Sample/Sample/ViewController.h
+++ Sample/Sample/ViewController.h
@@ -3,7 +3,7 @@
 //  Sample
 //
 //  Created by Ruslan Kavetsky on 2/14/13.
-//  Copyright (c) 2013 Ruslan. All rights reserved.
+//  Copyright (c) 2013 Ruslan Kavetsky. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
